5ca1d00af2ef5f1a790cb09873c3d1d74b1a0439
Eugene <jenyafomin4361@gmail.com>
2019-09-27 02:20:18 +0300

    Example img added, comments added in main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,4 @@
+// Include main lib file
 #include "MenuMaker.h"
 
 void setup(void) {
@@ -5,8 +6,11 @@ void setup(void) {
 
   // Init display size
   MenuMaker::Instance()->init( 123, 62 );
-  
+
+  // Create your own scene
   Scene* createdScene = MenuMaker::Instance()->createScene( "Garage" );
+
+  // Add buttons to this scene
   createdScene->addButton( "Open door", 
     [](){ Serial.print( "Door opened" ); } );
     
@@ -16,13 +20,20 @@ void setup(void) {
   createdScene->addButton( "<<< Back", 
     [](){ Serial.print( "To Previous menu" ); } );
 
+  // Display this or another scene
   MenuMaker::Instance()->showScene( "Garage" );
+
+  // Refresh screen rate (MS)
   MenuMaker::Instance()->setUpdateTimeMS( 2000 );
-  
+
+  // Call cursorUp and cursorDown methoods to move cursor
   createdScene->cursorDown();
+
+  // Current button callback
   createdScene->pressCurrentButton();
 }
 
 void loop(void) {
+  // Menu maker update
   MenuMaker::Instance()->update(millis());
 }
